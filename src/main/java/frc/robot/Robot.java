0bc38b20cb72f79b78de828086c995c6bc229aa3
Evan Chang <evanchang0515@gmail.com>
2026-01-16 18:17:11 -0800

    AutoSimTime (20 seconds)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -25,6 +29,7 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 public class Robot extends LoggedRobot {
   private Command autonomousCommand;
   private RobotContainer robotContainer;
+  private double autoSimTime = 20;
 
   public Robot() {
     // Record metadata
@@ -100,11 +105,22 @@ public class Robot extends LoggedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    autonomousCommand = robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
-      CommandScheduler.getInstance().schedule(autonomousCommand);
+    if (RobotBase.isSimulation()) {
+      Commands.waitSeconds(autoSimTime)
+          .andThen(
+              () -> {
+                DriverStationSim.setEnabled(false);
+                DriverStationSim.notifyNewData();
+              })
+          .onlyWhile(DriverStation::isAutonomousEnabled)
+          .schedule();
+      autonomousCommand = robotContainer.getAutonomousCommand();
+
+      // schedule the autonomous command (example)
+      if (autonomousCommand != null) {
+        CommandScheduler.getInstance().schedule(autonomousCommand);
+      }
     }
   }
 
